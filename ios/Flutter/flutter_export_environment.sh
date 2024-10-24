cd367d49f84e391c3f1faae767eaebcca94627e5
juanccalzadilla <juancalza91@gmail.com>
2024-10-24 18:54:34 +0200

    feat: Add web indicators to scroll

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter\easy_date_timeline"
+export "FLUTTER_ROOT=/Users/juancalzadilla/Desktop/Escritorio/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/juancalzadilla/Desktop/swimmerapp/plugins-mod/easy_date_timeline"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.2"
-export "FLUTTER_BUILD_NUMBER=1.0.2"
+export "FLUTTER_BUILD_NAME=1.0.4"
+export "FLUTTER_BUILD_NUMBER=1.0.4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
